1918f2cf4161a2fbda511357aea759ddd07c36a8
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-05-08 18:30:31 +0100

    update

--- src/components/Settings/Fonts.vue
+++ src/components/Settings/Fonts.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <p class="font-bold text-xl px-2 py-1">FONTS</p>
-        <div @click="selectFont(font)" v-for="(font, index) in allFonts " :key="index" class="p-1 py-3 border-b border-b-neutral-800 hover:bg-neutral-800">Dorayi typing test</div>
+        <p class="px-2 py-1 text-xl font-bold">FONTS</p>
+        <!-- <div @click="selectFont(font)" v-for="(font, index) in allFonts " :key="index" class="p-1 py-3 border-b border-b-neutral-800 hover:bg-neutral-800">Dorayi typing test</div> -->
     </div>
 </template>
 
