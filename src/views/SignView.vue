4b2727cf3dd7fa2380cd7e0d88f6fc1cffa0075a
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-05-29 11:20:43 +0100

    update

--- src/views/SignView.vue
+++ src/views/SignView.vue
@@ -9,20 +9,19 @@
 
         <p class="text-2xl">Coming soon...</p>
         
-        <div class="max-w-[400px] w-[90%]  space-y-3 m-auto  min-w-[350px] shadow shadow-black p-1 rounded-md px-2" >            
+        <div :class="[appTheme]" class="max-w-[400px] w-[90%]  space-y-3 m-auto  min-w-[350px] shadow shadow-black p-1 rounded-md px-2" >            
             <div class="flex items-center justify-between gap-3 m-auto">
                 <label  dir="rtl" class="font-mono w-fit" for="username">USERNAME</label>
-                <input id="username" placeholder="Username" type="text" class="inline-block w-full px-2 py-1 bg-transparent border-none outline-none appearance-none placeholder:text-sm placeholder:text-slate-400 text-slate-200">
+                <input id="username" placeholder="Username" type="text" class="inline-block w-full px-2 py-1 bg-transparent border-none outline-none appearance-none placeholder:text-sm placeholder:text-slate-400">
             </div>
             <div class="flex items-center justify-between gap-3 m-auto">
                 <label dir="rtl" class="font-mono w-fit" for="email">EMAIL</label>
-                <input id="email" placeholder="example@email.com" type="email" class="inline-block w-full px-2 py-1 bg-transparent border-none outline-none appearance-none placeholder:text-sm placeholder:text-slate-400 text-slate-200">
+                <input id="email" placeholder="example@email.com" type="email" class="inline-block w-full px-2 py-1 bg-transparent border-none outline-none appearance-none placeholder:text-sm placeholder:text-slate-400">
             </div>
             <div class="flex items-center justify-between gap-3 m-auto">
                 <label dir="rtl" class="font-mono w-fit" for="password">PASSWORD</label>
-                <input id="password" placeholder="Password" type="password" class="inline-block w-full px-2 py-1 bg-transparent border-none outline-none appearance-none placeholder:text-sm placeholder:text-slate-400 text-slate-200">
+                <input id="password" placeholder="Password" type="password" class="inline-block w-full px-2 py-1 bg-transparent border-none outline-none appearance-none placeholder:text-sm placeholder:text-slate-400">
             </div>
-            
             <div class="py-3 text-center">
                 <button class="px-10 py-1 font-mono border border-black rounded-full hover:bg-black hover:border-white hover:text-white">LOGIN</button>
             </div>
