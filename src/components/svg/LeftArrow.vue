d900019c3e1cde82a6c749e17968910624d5e365
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-06-22 15:32:34 +0100

    update

--- src/components/svg/LeftArrow.vue
+++ src/components/svg/LeftArrow.vue
@@ -1,4 +1,9 @@
 <template>
-    <div class="font-mono text-2xl font-bold hover:text-green-500">&lt;</div>
+    <div class="font-mono text-2xl font-bold hover:text-green-500" :class="[class]">&lt;</div>
 </template>
 
+<script setup>
+const props = defineProps({
+    class: String
+})
+</script>
--- src/components/svg/RightArrow.vue
+++ src/components/svg/RightArrow.vue
@@ -1,3 +1,9 @@
 <template>
-    <div class="font-mono text-2xl font-bold hover:text-green-500">&gt;</div>
+    <div class="font-mono text-2xl font-bold hover:text-green-500" :class="[class]">&gt;</div>
 </template>
+
+<script setup>
+const props = defineProps({
+    class: String
+})
+</script>
