d609a518c0dc2784035cec7938f213c2bf77385a
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-08-03 09:38:38 +0100

    update

--- src/components/CustomTestModal.vue
+++ src/components/CustomTestModal.vue
@@ -1,20 +1,20 @@
 <template>
-    <div v-if="customTestModal && route.name === 'home'" class="max-w-[500px] w-[100%] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-md py-5 max-h-[calc(100dvh_-_20px)]" :class="[theme === 'dark' ? 'bg-neutral-800' : 'bg-slate-300 shadow-sm shadow-slate-500']">
+    <div v-if="customTestModal && route.name === 'home'" class="max-w-[500px] w-[100%] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-md py-5 max-h-[calc(100dvh_-_20px)] text-[13px]" :class="[theme === 'dark' ? 'bg-neutral-800' : 'bg-slate-300 shadow-sm shadow-slate-500']">
         <div @click="customTestModal = !customTestModal, pauseTyping = false" class="absolute top-0 font-mono text-xl font-bold w-fit right-2 hover:text-red-500">x</div>
         <div>            
             <div v-if="!saveCustomText" class="space-y-1 text-center">
                 <p class="py-2 font-medium">Add your preferred test, quote or story.</p>
                 <textarea v-model="textValue" :class="[appTheme]" class="w-[90%] h-40 outline-none border border-slate-500 rounded-md p-2" :placeholder="textAreaPlaceholder" name="custom-testarea" />
-                <div @click="startSavingCustomText" class="py-[2px] m-auto text-center border rounded-full px-4 w-fit border-slate-600 hover:bg-green-500 hover:text-white text-xs font-medium">ADD TEST</div>
+                <div @click="startSavingCustomText" class="py-[2px] m-auto text-center border rounded-full px-4 w-fit border-slate-600 hover:bg-green-800 hover:text-white font-medium">ADD TEST</div>
                 <!-- <input type="file" name="" accept=".doc,.docx,.pdf,application/msword" id=""> -->
             </div>
             <div v-else class="pt-3 text-center">
                 <p v-if="textSaved" class="py-3 text-center text-green-600 uppercase">Added Successfully</p>
                 <div v-else class="space-y-4">                    
-                    <input name="custom-test-title" v-model="customTextTitle" type="text" :placeholder="titlePlaceholder" class="p-1 text-center bg-transparent border-b outline-none border-b-blue-500 placeholder:text-center">
-                    <div class="flex justify-center gap-2 m-auto text-xs font-medium w-fit">                    
+                    <input ref="inputTitleEl" name="custom-test-title" v-model="customTextTitle" type="text" :placeholder="titlePlaceholder" class="p-1 text-center bg-transparent border-b outline-none border-b-blue-500 placeholder:text-center">
+                    <div class="flex justify-center gap-2 m-auto text-xs font-medium w-fit">
                         <div @click="cancel" class="py-[2px] m-auto text-center border rounded-l-full px-4 w-fit border-slate-600 hover:bg-red-500 hover:text-white">CANCEL</div>
-                        <div @click="saveNewCustomText" class="py-[2px] m-auto text-center border rounded-r-full px-4 w-fit border-slate-600 hover:bg-green-500 hover:text-white">SAVE</div>
+                        <div @click="saveNewCustomText" class="py-[2px] m-auto text-center border rounded-r-full px-4 w-fit border-slate-600 hover:bg-green-800 hover:text-white">SAVE</div>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@
                 <div class="relative overflow-y-auto space-y-[5px]">                
                     <div v-for="(test, name, index) in customTests" :key="index" class="p-1 border hover:shadow-sm" :class="[theme === 'dark' ? 'border-neutral-700' : 'border-slate-400']">
                         <div class="text-xs font-medium uppercase">{{ name }} <span @click="use(name)" class="px-3 bg-green-600 rounded-full py-[1px] hover:bg-green-700 ml-1">use</span> <span v-if="index !== 0" @click="del(name)" class="px-3 py-[1px] bg-red-400 rounded-full hover:bg-red-500">delete</span></div>
-                        <div class="test-sm">{{ test }}</div>
+                        <div class="test-[10px]">{{ test }}</div>
                     </div>
                 </div>
             </div>
@@ -34,7 +34,7 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {onMounted, ref} from 'vue'
 import {storeToRefs} from 'pinia';
 import {mainStore} from '../store/mainStore';
 import {themeStore}  from '../store/themeStore'
@@ -52,6 +52,7 @@ const main = mainStore()
 const { customTests } = storeToRefs(main)
 const {switchNext} = main
 
+const inputTitleEl = ref(null)
 const textAreaPlaceholder = ref('Custom test')
 const titlePlaceholder = ref('Title')
 const textValue = ref('')
@@ -107,6 +108,7 @@ const del= (name) => {
     delete customTests.value[name]
     delete localStorageSettings.value.customTests[name]
     localStorage.setItem('dorayi-typing', JSON.stringify(localStorageSettings.value))
+    switchNext(customizers.value)
 }
 
 const use = (name) => {
@@ -122,5 +124,4 @@ const use = (name) => {
     customTestModal.value = false
     pauseTyping.value = false
 }
-
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="sticky top-0 mx-auto flex items-center justify-between pl-3 pr-2 max-w-[1500px] lg:mb-6 mb-3 min-[1350px]:fixed min-[1350px]:w-full text-[11px] uppercase font-medium h-[50px] py-2 min-[1350px]:z-[1]">
         <Logo class="flex" />
-        <div class="flex items-center gap-4 w-fit">
+        <div class="flex items-center gap-4 w-fit absolute" :class="[!hideElements ? 'min-[1350px]:right-3 min-[1350px]:translate-x-0 right-[50%] translate-x-[50%]' : 'left-[50%] translate-x-[-50%]']">
             <div v-if="route.name === 'home'"class="flex items-center gap-4 text-[9px]">
                 <div class="relative w-fit">
                     <Clock class="peer" />
@@ -38,6 +38,12 @@
             <div class="relative w-fit" v-if="route.name === 'home'" >
                 <settings class="w-5 peer" />
             </div>
+            <!-- <select name="" id="" class="text-black outline-none border-none uppercase text-center">
+                <option value="test">Test mode</option>
+                <option value="alphabet">Alphabet Mode</option>
+            </select> -->
+        </div>
+        <div>
         </div>
         <div :class="[!hideElements ? 'min-[1350px]:hidden' : '']" class="hidden min-[500px]:block"></div>
         <CompletionRange :class="[hideElements ? 'block' : 'hidden max-[460px]:block']" v-if="blind" />
@@ -49,6 +55,7 @@
 import Logo from './Logo.vue'
 import user from './svg/user.vue'
 import home from './svg/home.vue'
+import dropdown from './svg/dropdown.vue'
 import settings from './svg/settings.vue';
 import add from './svg/add.vue'
 import TextAlign from './TextAlign.vue';
