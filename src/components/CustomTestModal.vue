9cb2cc3edc53b53f8c0778bfa3231abb424c9939
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-08-05 20:34:45 +0100

    update

--- src/components/CustomTestModal.vue
+++ src/components/CustomTestModal.vue
@@ -1,17 +1,17 @@
 <template>
     <div v-if="customTestModal && route.name === 'home'" class="max-w-[500px] w-[100%] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-md py-5 max-h-[calc(100dvh_-_20px)] text-[13px]" :class="[theme === 'dark' ? 'bg-neutral-800' : 'bg-slate-300 shadow-sm shadow-slate-500']">
         <div @click="customTestModal = !customTestModal, pauseTyping = false" class="absolute top-0 font-mono text-xl font-bold w-fit right-2 hover:text-red-500">x</div>
+        
         <div>            
             <div v-if="!saveCustomText" class="space-y-1 text-center">
                 <p class="py-2 font-medium">Add your preferred test, quote or story.</p>
-                <textarea v-model="textValue" :class="[appTheme]" class="w-[90%] h-40 outline-none border border-slate-500 rounded-md p-2" :placeholder="textAreaPlaceholder" name="custom-testarea" />
+                <textarea v-model="textValue" :class="[appTheme]" class="w-[90%] h-40 outline-none border border-slate-500 rounded-md p-2" :placeholder="textAreaPlaceholder" name="custom-textarea" />
                 <div @click="startSavingCustomText" class="py-[2px] m-auto text-center border rounded-full px-4 w-fit border-slate-600 hover:bg-green-800 hover:text-white font-medium">ADD TEST</div>
-                <!-- <input type="file" name="" accept=".doc,.docx,.pdf,application/msword" id=""> -->
             </div>
             <div v-else class="pt-3 text-center">
                 <p v-if="textSaved" class="py-3 text-center text-green-600 uppercase">Added Successfully</p>
                 <div v-else class="space-y-4">                    
-                    <input ref="inputTitleEl" name="custom-test-title" v-model="customTextTitle" type="text" :placeholder="titlePlaceholder" class="p-1 text-center bg-transparent border-b outline-none border-b-blue-500 placeholder:text-center">
+                    <input name="custom-test-title" v-model="customTestTitle" type="text" :placeholder="titlePlaceholder" class="p-1 text-center bg-transparent border-b outline-none border-b-blue-500 placeholder:text-center">
                     <div class="flex justify-center gap-2 m-auto text-xs font-medium w-fit">
                         <div @click="cancel" class="py-[2px] m-auto text-center border rounded-l-full px-4 w-fit border-slate-600 hover:bg-red-500 hover:text-white">CANCEL</div>
                         <div @click="saveNewCustomText" class="py-[2px] m-auto text-center border rounded-r-full px-4 w-fit border-slate-600 hover:bg-green-800 hover:text-white">SAVE</div>
@@ -19,7 +19,8 @@
                 </div>
             </div>
         </div>
-        <div v-if="Object.keys(customTests).length" class="max-w-[90%] m-auto">
+        
+        <div class="max-w-[90%] m-auto">
             <div class="font-medium">CUSTOM TESTS</div>
             <div class="max-h-[calc(100dvh_-_50dvh)] overflow-y-auto h-fit p-1">
                 <div class="relative overflow-y-auto space-y-[5px]">                
@@ -35,11 +36,12 @@
                 </div>
             </div>
         </div>
+        
     </div>
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue'
+import {ref} from 'vue'
 import {storeToRefs} from 'pinia';
 import {mainStore} from '../store/mainStore';
 import {themeStore}  from '../store/themeStore'
@@ -57,13 +59,12 @@ const main = mainStore()
 const { customTests } = storeToRefs(main)
 const {switchNext} = main
 
-const inputTitleEl = ref(null)
 const textAreaPlaceholder = ref('Custom test')
 const titlePlaceholder = ref('Title')
 const textValue = ref('')
 const saveCustomText = ref(false)
 const textSaved = ref(false)
-const customTextTitle = ref('')
+const customTestTitle = ref('')
 const textAreaValueStore = ref('')
 
 const startSavingCustomText = () => {
@@ -85,16 +86,16 @@ const startSavingCustomText = () => {
 
 const saveNewCustomText = () => {
     const localStorageSettings = ref(JSON.parse(localStorage.getItem('dorayi-typing')))
-    if (!customTextTitle.value.length) {
+    if (!customTestTitle.value.length) {
         titlePlaceholder.value = 'Must add title'
         return
     }
 
-    customTests.value[customTextTitle.value] = textAreaValueStore.value.trim()
-    localStorageSettings.value.customTests[customTextTitle.value] = textAreaValueStore.value.trim()
+    customTests.value[customTestTitle.value] = textAreaValueStore.value.trim()
+    localStorageSettings.value.customTests[customTestTitle.value] = textAreaValueStore.value.trim()
     localStorage.setItem('dorayi-typing', JSON.stringify(localStorageSettings.value))
     textSaved.value = true
-    customTextTitle.value = ''
+    customTestTitle.value = ''
     textAreaValueStore.value = ''
 
     setTimeout(() => {
@@ -106,24 +107,28 @@ const saveNewCustomText = () => {
 const cancel = () => {
     saveCustomText.value = false
     textSaved.value = false
-    customTextTitle.value = ''
+    customTestTitle.value = ''
 }
 
 const del= (name) => {
-    const localStorageSettings = ref(JSON.parse(localStorage.getItem('dorayi-typing')))
+    if (localStorage.getItem('dorayi-typing')) {        
+        const localStorageSettings = ref(JSON.parse(localStorage.getItem('dorayi-typing')))
+        delete localStorageSettings.value.customTests[name]
+        localStorage.setItem('dorayi-typing', JSON.stringify(localStorageSettings.value))
+    }
     delete customTests.value[name]
-    delete localStorageSettings.value.customTests[name]
-    localStorage.setItem('dorayi-typing', JSON.stringify(localStorageSettings.value))
     switchNext(customizers.value)
 }
 
 const use = (name) => {
     if (repeat.value) return
-    const localStorageSettings = ref(JSON.parse(localStorage.getItem('dorayi-typing')))
-    if (customizers.value['test-type'] !== 'custom-test') {
-        customizers.value['test-type'] = 'custom-test'
-        localStorageSettings.value.config[0]['test-type'] = 'custom-test'
-        localStorage.setItem('dorayi-typing', JSON.stringify(localStorageSettings.value))
+    if (localStorage.getItem('dorayi-typing')) {        
+        const localStorageSettings = ref(JSON.parse(localStorage.getItem('dorayi-typing')))
+        if (customizers.value['test-type'] !== 'custom-test') {
+            customizers.value['test-type'] = 'custom-test'
+            localStorageSettings.value.config[0]['test-type'] = 'custom-test'
+            localStorage.setItem('dorayi-typing', JSON.stringify(localStorageSettings.value))
+        }
     }
     selectedCustomTest.value = name
     switchNext(customizers.value)
