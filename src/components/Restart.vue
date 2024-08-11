4d744c53131e05c927e936744a79561752dd411e
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-08-11 18:19:31 +0100

    update

--- src/components/Restart.vue
+++ src/components/Restart.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pt-4 mx-auto w-fit h-fit">        
+    <div class="pt-4 mx-auto w-fit h-fit font-[400]">        
         <div ref="restartEl" v-show="route.name == 'home' || route.name == 'result'" class="m-auto text-sm transition-all duration-100 text-slate-500">
             <div v-show="isMobileOS()" @click="next" class="p-1 m-auto text-base text-center border rounded-md w-fit border-neutral-600">NEXT</div>
             <div v-show="!isMobileOS()" class="m-auto text-center cursor-default w-fit h-fit">Press <span @click="next" class="p-1 px-3 text-xs text-white rounded-md cursor-pointer bg-neutral-800 hover:bg-green-600">{{ hasCompletedSession ? 'Enter' : 'Esc' }}</span> for NEXT</div>
