ea8c9fff7315584b0e988820a426d9c5c36e6d72
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-08-11 18:30:42 +0100

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,10 +6,11 @@
         <p class="text-center text-[50px] leading-0 opacity-90 blur-[1px]">KiBoard</p>
         <div class="m-auto  font-normal w-fit z-[-1]">
             <p class="pt-3 m-auto text-center w-fit">IS A TYPING APP CREATED BY AHMED ABDULRAHMAN</p>
-            <div class="justify-center flex max-w-[700px] gap-5 m-auto py-5 pt-7 flex-wrap text-sm">
-                <div @click="name='github'" class="px-2 py-[1px] border-gray-500 border rounded-sm transition-all duration-100 hover:shadow-md hover:border-green-500 flex items-center gap-2"><github class="w-4" /> GITHUB</div>
-                <div @click="name='linkedin'" class="px-2 py-[1px] border-gray-500 border rounded-sm transition-all duration-100 hover:shadow-md hover:border-green-500 flex items-center gap-2"><linkedin class="w-6" /> LINKEDIN</div>
-                <div @click="name='email'" class="px-2 py-[1px] border-gray-500 border rounded-sm transition-all duration-100 hover:shadow-md hover:border-green-500 flex items-center gap-2"><email class="w-4" /> EMAIL</div>
+            <p class="pt-2 text-center underline">CONTACT ME</p>
+            <div class="justify-center flex max-w-[700px] gap-5 m-auto py-5 pt-1 flex-wrap text-sm">
+                <!-- <div @click="name='github'" class="px-2 py-[1px] border-gray-500 border rounded-sm transition-all duration-100 hover:shadow-md hover:border-green-500 flex items-center gap-2"><github class="w-4" /> GITHUB</div>
+                <div @click="name='linkedin'" class="px-2 py-[1px] border-gray-500 border rounded-sm transition-all duration-100 hover:shadow-md hover:border-green-500 flex items-center gap-2"><linkedin class="w-6" /> LINKEDIN</div> -->
+                <div @click="name='email'" class="px-2 py-[1px] border-gray-500 border rounded-sm transition-all duration-100 hover:shadow-md hover:border-green-500 flex items-center gap-2"><email class="w-4" />EMAIL</div>
             </div>
         </div>
         <div class="m-auto font-normal w-fit">
@@ -36,7 +37,7 @@ const name = ref('')
 watch(name, (newVal) => {
     if (newVal) {
         if (newVal === 'email') {
-            window.open(`mailto:ahmedabdulrahman103@gmail.com`)
+            window.open(`mailto:ahmadabdulrahman103@gmail.com`)
         }
         if (newVal === 'github') {
             window.open('https://github.com/abdurrahmanu', '_blank')
