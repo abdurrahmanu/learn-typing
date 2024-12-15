099660a06d49ba314b1e34ff02bb62c8724a7a48
Abdur-Rahman <ahmadabdulrahman103@gmail.com>
2024-12-15 17:18:35 +0100

    update

--- src/components/Settings/Header.vue
+++ src/components/Settings/Header.vue
@@ -1,8 +1,9 @@
 <template>
     <div class=" sticky z-[99999] top-[0] bg-neutral-800 text-slate-200">
-        <div  class="flex items-center justify-between w-full gap-4 px-2 text-right border-r shadow-sm border-r-zinc-700 shadow-black">
-            <div class="p-2 text-lg ">SETTINGS</div>
-            <div class="text-white font-bold  text-lg absolute top-0 bottom-0 right-0 p-2 hover:cursor-default" @click="toggleSettings">
+        <div  class="flex items-center w-full px-2 relative justify-between">
+            <div class="p-2 text-lg">SETTINGS</div>
+            <!-- <SearchBar /> -->
+            <div class="text-white font-bold  text-lg p-2 hover:cursor-default" @click="toggleSettings">
                 <p class="font-mono  w-fit hover:text-red-500 hover:cursor-pointer">X</p>
             </div>
         </div>
@@ -12,6 +13,7 @@
 <script setup>
 import {storeToRefs} from 'pinia'
 import { customizeStore } from '../../store/customizeStore';
+import SearchBar from './SearchBar.vue';
 
 const custom = customizeStore()
 const { allSettings } = storeToRefs(custom)
---
+++ src/components/Settings/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex items-center max-w-[50%]">
+        <input v-model="searchValue" type="text" placeholder="search" class="outline-none text-xs appearance-none px-3 py-1 bg-transparent border-b border-b-white w-full">
+        <div @click="searchSettings(searchValue)" class="w-3 h-3 rounded-full bg-green-400"></div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const searchValue = ref('')
+const allSettings = [
+
+]
+
+const searchSettings = (val) => {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
