f4c103a5ab0eba25ae1a6048fb1d4bd5fd361da0
abdurrahmanu <ahmadabdulrahman103@gmail.com>
2025-07-20 18:48:05 +0100

    update

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="relative cursor-default w-fit h-fit p-[1px]">
-        <span class="absolute top-0 left-[50%] translate-x-[-50%] text-[11px] font-black pt-[1px]"><span class="hover:text-green-500">K</span><span class="hover:text-blue-500">I</span></span>
+        <span class="absolute top-0 left-[50%] translate-x-[-50%] text-[9px] font-black pt-[1px]"><span class="hover:text-green-500">K</span><span class="hover:text-blue-500">I</span></span>
         <board :class="[isMobile() ? 'w-5' : 'w-6']" />
     </div>
 </template>
