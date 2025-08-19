01728c063d634f93ee35a84c6509c28c1f76998c
abdurrahmanu <ahmadabdulrahman103@gmail.com>
2025-08-19 20:43:43 +0100

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import path from 'path'
 
-const path = require('path')
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   path: path.resolve(__dirname, './dist'),
